28f4dd510d048948b938cf28b7c152f917055dfd
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-10-17 12:51:47 +0300

    first commit

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,76 @@
+const sliders = (slides, dir, prev, next) => {
+  //dir - direction sliders (vertical or horizontal)
+
+  let slideIndex = 1;
+  let paused = false;
+
+  const items = document.querySelectorAll(slides);
+
+  function showSlides(n) {
+    if (n > items.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = items.length;
+    }
+
+    //all slides are hidden
+    items.forEach((item) => {
+      item.classList.add("animated");
+      item.style.display = "none";
+    });
+
+    items[slideIndex - 1].style.display = "block";
+  }
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  try {
+    const prevBtn = document.querySelector(prev),
+      nextvBtn = document.querySelector(next);
+
+    prevBtn.addEventListener("click", () => {
+      plusSlides(-1);
+      items[slideIndex - 1].classList.remove("slideInLeft");
+      items[slideIndex - 1].classList.add("slideInRight");
+    });
+
+    nextvBtn.addEventListener("click", () => {
+      plusSlides(1);
+      items[slideIndex - 1].classList.remove("slideInRight");
+      items[slideIndex - 1].classList.add("slideInLeft");
+    });
+  } catch (e) {}
+
+  function activatedAnimation() {
+    if (dir === "vertical") {
+      paused = setInterval(function() {
+        plusSlides(1);
+        items[slideIndex - 1].classList.add("slideInDown");
+      }, 3000);
+    } else {
+      paused = setInterval(function() {
+        plusSlides(1);
+        items[slideIndex - 1].classList.remove("slideInRight");
+        items[slideIndex - 1].classList.add("slideInLeft");
+      }, 3000);
+    }
+  }
+  activatedAnimation();
+
+  //hover the mouse over the slider and it stops
+  items[0].parentNode.addEventListener("mouseenter", () => {
+    clearInterval(paused);
+  });
+
+  //remove the mouse from the slider and the automatic slider works
+  items[0].parentNode.addEventListener("mouseleave", () => {
+    activatedAnimation();
+  });
+};
+
+export default sliders;
